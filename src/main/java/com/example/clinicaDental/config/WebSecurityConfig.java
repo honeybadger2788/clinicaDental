81d97876d087fa6524aa1e4d2877a5298fd8543c
honeybadger2788 <noe.carosella@gmail.com>
2022-07-11 11:17:16 -0300

    add jwt authorization

--- src/main/java/com/example/clinicaDental/config/WebSecurityConfig.java
+++ src/main/java/com/example/clinicaDental/config/WebSecurityConfig.java
@@ -1,13 +1,32 @@
 package com.example.clinicaDental.config;
 
+import com.example.clinicaDental.jwt.JwtRequestFilter;
+import com.example.clinicaDental.service.impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -22,6 +41,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().formLogin()
                 // estos dos se usan juntos si quiero utilizar cookies
                 .and().rememberMe().key("uniqueAndSecret")
-                .and().logout().deleteCookies("JSESSIONID");
+                .and().logout().deleteCookies("JSESSIONID")
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
